f55cd5e4297a9ff387baab2ac5432bff134390a9
Green Man <brendan.mccaughey@gmail.com>
2018-10-22 16:03:25 -0400

    Each new button click refreshes the page and only displays those gifs.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GiphyAPI_Project
 Project utilizing the Giphy API
+
+Working
+==================
+- buttons displaying correctly
+- gifs displaying with their ratings on button click
+- each button click displays 10 gifs above the previous gifs
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
 let topics = ['mario', 'kirby', 'the legend of zelda', 'metroid', 'mega man', 'battletoads', 'ninja gaiden', 'contra', 'double dragon', 'bubble bobble', 'castlevania', 'tetris',];
+let storedGifs = [];
 
     //Loop to create buttons and add text/attribute
     for (i=0; i <topics.length; i++) {
         var gifButton = $('<button>');
-        gifButton.addClass('findGifs');
+        gifButton.addClass('findGifs btn btn-info');
         gifButton.attr('data-value', topics[i]);
         gifButton.text(topics[i]);
         $('#buttonArea').append(gifButton);
@@ -13,6 +14,7 @@ let topics = ['mario', 'kirby', 'the legend of zelda', 'metroid', 'mega man', 'b
 
     //On click event to search GIPHY and display results below buttons
     $(document).on('click', '.findGifs', function() {
+        $('#gifArea').empty();
         var search = $(this).attr('data-value');
         //Query URL
         var xhr = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=FbiNAP9uph7PGhtFufRqkGRI1T9PU560&limit=10"
@@ -33,7 +35,7 @@ let topics = ['mario', 'kirby', 'the legend of zelda', 'metroid', 'mega man', 'b
         var stillGif = results[i].images.fixed_height_still.url;
         var gifDisplay = $('<div>');
         var gifImage = $('<img>');
-        var p = $('<p>').text('Rating: ' + rating)
+        var p = $('<p>').text('Rating: ' + rating);
         gifDisplay.addClass('display');
         gifImage.attr('src', stillGif);
         gifImage.addClass('start-stop');
@@ -42,6 +44,15 @@ let topics = ['mario', 'kirby', 'the legend of zelda', 'metroid', 'mega man', 'b
         $('#gifArea').prepend(gifDisplay);
         }
     });
+    });
+    //add ability to click on the image and have it switch from still image to moving gif
+    //start with on click function like above but targetting class added to images
+    $(document).on('click', '.start-stop', function() {
+        //create a variable to store the object path to the moving versions of the gif
+        var playGif = response.data.images.fixed_height.url;
+        //Change src attribute to variable 
+        $('.start-stop').attr('src', playGif);
+        //currently not recognizing variable. I think I need to make another ajax call to have access to the api data
     });
 
 
